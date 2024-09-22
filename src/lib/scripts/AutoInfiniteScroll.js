5dbbf172d7e94e8fa500a30b5c852f7d35d2722e
ihsan.onlinefs <ihsan.onlinefs@gmail.com>
2024-09-22 03:25:25 +0000

    git push

--- src/lib/scripts/AutoInfiniteScroll.js
+++ src/lib/scripts/AutoInfiniteScroll.js
@@ -1,20 +1,20 @@
 import gsap from "gsap";
 
-gsap.utils.toArray('.auto_scroll').forEach((line, i) => {
-
-    const speed = 200
-
-    const links = line.querySelectorAll("a"),
-        tl = verticalLoop(links, i ? -speed : speed)
-
+gsap.utils.toArray('.autoscroll').forEach((line, i) => {
+  
+    const speed = 150
+    
+    const links = line.querySelectorAll(".linkscroll"),
+          tl = verticalLoop(links, i ? -speed : speed)
+      
     links.forEach(link => {
-        link.addEventListener("mouseenter", () => gsap.to(tl, { timeScale: 0, overwrite: true }));
-        link.addEventListener("mouseleave", () => gsap.to(tl, { timeScale: 1, overwrite: true }));
+      link.addEventListener("mouseenter", () => gsap.to(tl, {timeScale: 0, overwrite: true}));
+      link.addEventListener("mouseleave", () => gsap.to(tl, {timeScale: 1, overwrite: true}));
     });
+    
+  });
 
-});
-
-function verticalLoop(elements, speed) {
+  function verticalLoop(elements, speed) {
     elements = gsap.utils.toArray(elements);
     let firstBounds = elements[0].getBoundingClientRect(),
         lastBounds = elements[elements.length - 1].getBoundingClientRect(),
@@ -22,28 +22,28 @@ function verticalLoop(elements, speed) {
         right = lastBounds.right,
         distance = right - left,
         duration = Math.abs(distance / speed),
-        tl = gsap.timeline({ repeat: -1 }),
+        tl = gsap.timeline({repeat: -1}),
         plus = speed < 0 ? "-=" : "+=",
         minus = speed < 0 ? "+=" : "-=";
     elements.forEach(el => {
-        let bounds = el.getBoundingClientRect(),
-            ratio = Math.abs((right - bounds.left) / distance);
-        if (speed < 1) {
-            ratio = 1 - ratio;
-        }
-        tl.to(el, {
-            x: plus + distance * ratio,
-            duration: duration * ratio,
-            ease: "none"
-        }, 0);
-        tl.fromTo(el, {
-            x: minus + distance
-        }, {
-            x: plus + (1 - ratio) * distance,
-            ease: "none",
-            duration: (1 - ratio) * duration,
-            immediateRender: false
-        }, duration * ratio)
+      let bounds = el.getBoundingClientRect(),
+          ratio = Math.abs((right - bounds.left) / distance);
+      if (speed < 1) {
+        ratio = 1 - ratio;
+      }
+      tl.to(el, {
+        x: plus + distance * ratio,
+        duration: duration * ratio,
+        ease: "none"
+      }, 0);
+      tl.fromTo(el, {
+        x: minus + distance
+      }, {
+        x: plus + (1 - ratio) * distance,
+        ease: "none",
+        duration: (1 - ratio) * duration,
+        immediateRender: false
+      }, duration * ratio)
     });
     return tl;
-}
+  }
--- src/lib/scripts/scrollSmooth.js
+++ src/lib/scripts/scrollSmooth.js
@@ -1,23 +1,13 @@
 import "../../styles/lenis.css";
 import Lenis from "lenis";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(ScrollTrigger);
 const lenis = new Lenis();
 
-lenis.on("scroll", (e) => {
-console.log(e);
-});
-lenis.on('scroll', ScrollTrigger.update)
-
-gsap.ticker.add((time)=>{
-  lenis.raf(time * 4000)
-})
-gsap.ticker.lagSmoothing(0)
-
 function raf(time) {
-lenis.raf(time);
-requestAnimationFrame(raf);
+    lenis.raf(time);
+    requestAnimationFrame(raf);
 }
 
 requestAnimationFrame(raf);
